750d2f6d2d3f3db290cd4dc159edf820cf4921bf
ChavleskaEmanuela <116969263+ChavleskaEmanuela@users.noreply.github.com>
2024-11-05 22:31:20 +0100

    Home Page

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {assets} from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr] gap-14 my-10 mt-40 text-sm'>
+
+        {/* ----- Left Section ------ */}
+        <div>
+            <img className='mb-5 w-40' src={assets.logo} alt="" />
+            <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+
+         {/* ----- Right Section ------ */}
+        <div>
+            <p className='text-xl font-medium mb-5 mt-8'>COMPANY</p>
+            <ul className='flex flex-col gap-2 text-gray-600'>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Contact us</li>
+                <li>Privacy policy</li>
+            </ul>
+         </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer

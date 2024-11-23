4b4830396c1c6a972c3ea41e35773fb482fca06f
ChavleskaEmanuela <116969263+ChavleskaEmanuela@users.noreply.github.com>
2024-11-23 20:48:39 +0100

    final changes

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 
+
 const Banner = () => {
 
     const navigate = useNavigate()
+    const token = localStorage.getItem('token')
   return (
+    
     <div className='flex bg-primary rounded-lg px-6 sm:px-10 md:px-14 lg:px-12 my-20 md:mx-10'>
         {/* ------- Left Side ------ */}
         <div className='flex-1 py-8 sm:py-10 md:py-16 lg:py-24 lg:pl-5'>
@@ -13,7 +16,19 @@ const Banner = () => {
                 <p>Book Appointment</p>
                 <p className='mt-4'>With 100+ Trusted Doctors</p>
             </div>
-            <button onClick={()=>{navigate('/login'); scrollTo(0,0)}} className='bg-white text-sm sm:text-base text-grap-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all'>Create account</button>
+
+            {!token && (
+          <button
+            onClick={() => {
+              navigate('/login')
+              window.scrollTo(0, 0) // Ensure the page scrolls to the top when navigating
+            }}
+            className='bg-white text-sm sm:text-base text-grap-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all'
+          >
+            Create account
+          </button>
+        )}
+              
         </div>
 
           {/* ------- Right Side ------ */}
